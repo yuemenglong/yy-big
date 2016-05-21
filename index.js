9d90a994bd60cab5e6a6179b01e73a5875418079
yml <yuemenglong@126.com>
2016-05-21 17:56:18 +0800

    export BigNumber

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var json_stringify = require('./lib/stringify.js').stringify;
 var json_parse     = require('./lib/parse.js');
+var BigNumber      = require('bignumber.js');
 
 module.exports = function(options) {
     return  {
@@ -10,3 +11,8 @@ module.exports = function(options) {
 //create the default method members with no options applied for backwards compatibility
 module.exports.parse = json_parse();
 module.exports.stringify = json_stringify;
+module.exports.JSON = {
+	parse: json_parse(),
+	stringify: json_stringify
+}
+module.exports.Number = BigNumber;
